5c80abb9a0def2d9203de55ad3159fac8a45ec5f
lrh <153011490@qq.com>
2019-07-01 01:37:25 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,10 @@
 -- 插入记录  
-
+insert into student values('001','张三',18,'男');
+insert into student values('002','李四',20,'女');
 -- 修改记录  
-
---  删除记录  
-
+update student set name = '王一' where id = '001';
+--  删除记录 
+delete from student where id = '002';
 -- 查询记录  
+select * from student where name = '王一';
+select age from student where id = '001';
